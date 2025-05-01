1477eb90a2d29813bc7437ab642b17e8a1a80694
root <marcel.imanuel1211@gmail.com>
2025-05-01 18:08:39 +0700

    Belajar

---
+++ Belajar_Cpp/tipe_data.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string nama = "Marcel";
+    int umur = 19;
+    char jenis_Kelamin = 'L';
+    float tinggi = 1.75;
+    double berat = 60.5;
+    bool status = true;
+    
+    cout << "Nama : " << nama << endl;
+    cout << "Umur : " << umur << endl;
+    cout << "Jenis Kelamin : " << jenis_Kelamin << endl;
+    cout << "Tinggi : " << tinggi << endl;
+    cout << "Berat : " << berat << endl;
+    cout << "Status : " << status << endl;
+}
